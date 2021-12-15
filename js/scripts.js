5b22cd3d984316ebb325e77ed10e98e0adc5469e
John Whitten <johnwhitten.studio@gmail.com>
2021-12-14 23:46:18 -0800

    update html, js, and css after getting things to function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,226 +1,193 @@
-
-// Business Logic for Orders ---------
-function Order() {
-  this.pizzas = {};
-  this.currentPizzaId = 0;
-  this.totalDue = 0
+// Business Logic for AddressBook ---------
+function AddressBook() {
+  this.contacts = {};
+  this.currentId = 0;
 }
 
-Order.prototype.addPizza = function(pizza) {
-  pizza.id = this.assignId();
-  this.pizzas[pizza.id] = pizza;
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
 };
 
-Order.prototype.assignId = function() {
-  this.currentPizzaId += 1;
-  return this.currentPizzaId;
-};
-
-Order.prototype.findPizza = function(id) {
-  if (this.pizzas[id] != undefined) {
-    return this.pizzas[id];
-  }
-  return false;
-};
-
-Order.prototype.deleteOrder = function(id) {
-  if (this.pizzas[id] === undefined) {
-    return false;
-  }
-  delete this.orders[id];
-  return true;
-};
-
-// Business Logic for Customer ---------
-function Customer(firstName, lastName, phoneNumber, address1, address2, city) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.phoneNumber = phoneNumber;
-  this.address1 = address1;
-  this.address2 = address2;
-  this.city = city;
-  this.addresses = {};
-  this.currentId = -1;  
-};
-
-Customer.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-};
-
-Customer.prototype.addAddress = function(address){
-  address.id = this.assignId();
-  this.addresses[address.id] = address;
-}
-
-Customer.prototype.assignId = function() {
+AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 };
 
-Customer.prototype.findAddress = function(id){
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] != undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+};
+
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+  this.addresses = {};
+  this.currentId = -1;  
+};
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
+
+Contact.prototype.addAddress = function(address){
+  address.id = this.assignId();
+  this.addresses[address.id] = address;
+}
+
+Contact.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+Contact.prototype.findAddress = function(id){
   if (this.addresses[id] != undefined) {
     return this.addresses[id];
   }
   return false;
 };
+//business logic for Addresses
 
-// Business logic for Pizzza
-
-function Pizza(type, size, sauce, toppings, price){
+function Address(address, city, type, crust, size, sauce){
+  this.address = address;
+  this.city = city;
   this.type = type;
+  this.crust = crust;
   this.size = size;
   this.sauce = sauce;
-  this.toppings = toppings;
-  this.price = 16.00;
 }
-
-Pizza.prototype.listToppings = function () {
-  return this.toppings.join(", ");
-}
-
-Pizza.prototype.calculatePrice = function() {
-  this.toppings.forEach(topping => {
-    if (topping === "mozarella" || topping === "pepperoni" || topping === "peppers" || topping === "feta" || topping === "tomatoes" || topping === "garlic") {
-      this.price += 2.00;
-    }
-  });
-  if (this.size === "MD") {
-    this.price += 8.00;
-  }
-  if (this.size === "LG") {
-    this.price += 16.00;
-  }
-  if (this.size === "XL") {
-    this.price += 24.00;
-  }
-}
-
 
 //other business logic
-function displayOrderDetails(orderToDisplay){
-  let orderConfirmation = $("ul#orders");
-  let htmlForOrderConfirmation = "";
-  Object.keys(orderToDisplay.pizzas).forEach(function(key){
-    const pizza = orderToDisplay.findPizza(key);
-    htmlForOrderConfirmation += "<li id=" + customer.id + ">" + customer.firstName + " " + customer.lastName + "</li>";
+function displayContactDetails(addressBookToDisplay){
+  let contactList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key){
+    const contact = addressBookToDisplay.findContact(key);
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName +"</li>";
   });
-  orderConfirmation.html(htmlForOrderConfirmation); 
+  contactList.html(htmlForContactInfo); 
 }
 
-function findFirstPie(order){
-  let isFirst = false;
-  Object.values(order).forEach(function(element){
-    if(element === "first")
-    isFirst = true;
+function findPersonal(address){
+  let isPersonal = false;
+  Object.values(address).forEach(function(element){
+    if(element === "personal")
+    isPersonal = true;
   })
-  return isFirst;
+  return isPersonal;
 }
 
-function showFirst(pizza){    
-  $(".size").html(pizza.size);
-  $(".sauce").html(pizza.sauce);
-  $(".toppings").html(pizza.toppings);
-  $(".price").html(pizza.price);
+function showPersonal(address){ 
+  $(".address").html(address.address);
+  $(".city").html(address.city);   
+  $(".crust").html(address.crust);
+  $(".size").html(address.size);
+  $(".sauce").html(address.sauce);
 }
 
-function showSecond(pizza){
-  $(".second-size").html(pizza.size);
-  $(".second-sauce").html(pizza.sauce);
-  $(".second-toppings").html(pizza.toppings);
-  $(".price").html(pizza.price);
+function showBusiness(address){
+  // $(".business-address").html(address.address);
+  // $(".business-city").html(address.city);
+  $(".business-crust").html(address.crust);
+  $(".business-size").html(address.size);
+  $(".business-sauce").html(address.sauce);
 }
-
-function showCustomer(customerID){
-  const customer = order.findCustomer(customerID);
-  $("#show-customer").show();
-  $(".first-name").html(customer.firstName);
-  $(".last-name").html(customer.lastName);
-  $(".address1").html(customer.address1);
-  $(".address2").html(customer.address2);
-  $(".city").html(customer.city);
+function showContact(contactID){
+  const contact = addressBook.findContact(contactID);
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
   let index = 0;
-  while(customer.orders[index] != null){
-    let isFirst = findFirst(customer.orders[index]);
-    if(isFirst){
-      showFirst(customer.orders[index]);
+  while(contact.addresses[index] != null){
+    let isPersonal = findPersonal(contact.addresses[index]);
+    if(isPersonal){
+      showPersonal(contact.addresses[index]);
     } else{
-      showSecond(customer.orders[index]);
+      showBusiness(contact.addresses[index]);
     }
     index++;
   }
   let buttons = $("#buttons");
   buttons.empty();
-  buttons.append("<button class='deleteButton' id=" + customer.id + ">Delete</button>");
+  buttons.append("<button class='deleteButton' id=" + contact.id + ">Delete</button>");
 }
 
-function containsType(pizza, type){
-  if(pizza.type === type){
+function containsType(address, type){
+  if(address.type === type){
     return true;
   }
   return false;
 }
-function attachCustomerListeners(){
-  $("ul#customers").on("click", "li", function(){
-    showCustomer(this.id);
+function attachContactListeners(){
+  $("ul#contacts").on("click", "li", function(){
+    showContact(this.id);
   });
   $("#buttons").on("click", ".deleteButton", function() {
-    order.deleteCustomer(this.id);
-    $("#show-customer").hide();
-    displayCustomerDetails(order);
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
   });
 }
 
 function emptyForm(){
-  $("input#new-first-name").val("");
-  $("input#new-last-name").val("");
-  $("input#new-phone-number").val("");
-  $("input#new-address-1").val("");
-  $("input#new-address-2").val("");
-  $("select#new-city").val("");
+  // $("input#new-first-name").val("");
+  // $("input#new-last-name").val("");
+  // $("input#new-phone-number").val("");
+  // $("input#new-address").val("");
+  // $("select#new-city").val("");
+  $("select#new-crust").val("");
   $("select#new-size").val("");
   $("select#new-sauce").val("");
-  $("input#new-toppings").val("");
 }
-
-
 //ui logic
 
 
-let order = new Order();
-
-
-
+let addressBook = new AddressBook();
 $(document).ready(function(){
 
-  attachCustomerListeners();
-  let currentCustomer = new Customer("", "")
+  attachContactListeners();
+  let currentContact = new Contact("", "")
 
-  $("form#new-customer").submit(function(event){
+  $("form#new-contact").submit(function(event){
     event.preventDefault();
-    order.addCustomer(currentCustomer);
-    displayCustomerDetails(order);
+    addressBook.addContact(currentContact);
+    displayContactDetails(addressBook);
     emptyForm();
   });
 
-  $("#addOrder").on("click", function(){
+  $("#addAddress").on("click", function(){
     
     const inputtedFirstName = $("input#new-first-name").val();
     const inputtedLastName = $("input#new-last-name").val();
     const inputtedPhoneNumber = $("input#new-phone-number").val();
-    const inputtedAddress = $("input#new-address-1").val();
-    const inputtedAddress2 = $("input#new-address-2").val();
-    const selectedCity = $("select#new-city").val();
-    const selectedType = $("input[name='pizza-order']:checked").val();
-    const selectedSize = $("input[name='pizza-order']:checked").val();
-    const selectedSauce = $("input[name='pizza-order']:checked").val();
-    const inputtedToppings = $("input:checkbox[name='toppings']:checked").each(function() {
-    yourArray.push($(this).val());
-  });
-    let newOrder = new Order(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedAddress, inputtedAddress2, selectedCity, selectedType, selectedSize, selectedSauce, inputtedToppings);
+    const inputtedAddress = $("input#new-address").val();
+    const inputtedCity = $("input#city").val();
+    const selectedType = $("input[name='address-type']:checked").val();
+    const selectedCrust = $("select#new-crust").val();
+    const selectedSize = $("select#new-size").val();
+    const selectedSauce = $("select#new-sauce").val();
+    let newAddress = new Address(inputtedAddress, inputtedCity, selectedType, selectedCrust, selectedSize, selectedSauce);
 
-    currentCustomer.firstName = inputtedFirstName;
-    currentCustomer.lastName = inputtedLastName;
-    currentCustomer.addOrder(newOrder);
+    currentContact.lastName = inputtedLastName;
+    currentContact.firstName = inputtedFirstName;
+    currentContact.phoneNumber = inputtedPhoneNumber;
+    currentContact.addAddress(newAddress);
     emptyForm();
   });
 });
+
+//playground
+
